687c1b531acb36cd9ef64af4d2f7ba63f9f8eaa1
Sarah Bingham <sarah@sarah-bingham.io>
2023-04-16 22:44:10 -0500

    Update actions versions; remove terraform provider versions

--- main.tf
+++ main.tf
@@ -12,11 +12,9 @@ terraform {
 provider "aws" {
   region  = var.region
   alias   = "main"
-  version = "~> 3.0"
 }
 
 provider "aws" {
   region  = "us-east-1"
   alias   = "acm"
-  version = "~> 3.0"
 }
